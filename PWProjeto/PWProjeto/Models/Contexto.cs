5d3ad4ae1b94576d0c727aaa9a5505660c949319
KAIQUE PEREIRA <kaique.pereira01@fatec.sp.gov.br>
2018-06-22 01:02:28 -0300

    Criando classes do banco e DAO de cliente

---
+++ PWProjeto/PWProjeto/Models/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PWProjeto.Models
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class Contexto:DbContext
+    {
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+        public virtual DbSet<Cliente> Clientes { get; set; }
+        public Contexto()
+            :base("conexaoString")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
