9b216bc939dcc9b2c0ac7bf2d675b5e15ec1ad74
KAIQUE PEREIRA <kaique.pereira01@fatec.sp.gov.br>
2018-06-22 14:21:41 -0300

    Adicionando solução que funciona a conexao com o banco. Criada lista.aspx

---
+++ PWProjeto/WebApplication1/Models/Contexto.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication1.Models
+{
+
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class Contexto:DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public Contexto()
+            :base("conexaoString")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<Cliente>().MapToStoredProcedures();
+
+            modelBuilder.Entity<Cliente>().ToTable("cliente");
+            modelBuilder.Entity<Cliente>().HasKey(x => x.cod_cliente);
+
+        }
+    }
+}
